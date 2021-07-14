7ebe4892daa587b0975e408d00c00e20839966f8
Aung Phone Khant <morriszin016@gmail.com>
2021-07-14 20:25:39 +0630

    implemented two reducers productDetails and productList

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
-const reducers = combineReducers({ productList: productListReducer });
+const reducers = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 const middleware = [thunk];
 
 const store = createStore(
